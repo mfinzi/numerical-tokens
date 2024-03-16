6fd6db35ccb8f5927c377717385fd38603841a54
Dylan <dylansam@andrew.cmu.edu>
2024-03-16 11:42:01 -0400

    zs graph results with naive text serialization

--- graph_datasets.py
+++ graph_datasets.py
@@ -8,6 +8,10 @@ import json
 
 class ArxivDataset(Dataset):
 
+    '''
+    Dataset from LLaGA -> larger graphs, probably out of scope
+    '''
+
     def __init__(self, root, split="train"):
         self.dataset = NodePropPredDataset(name = "ogbn-arxiv")
         self.split_idx = self.dataset.get_idx_split()
@@ -52,20 +56,63 @@ class ArxivDataset(Dataset):
 
 class NLGraph(Dataset):
 
-    def __init__(self, root="./NLGraph", task="connectivity", split="train"):
+    def __init__(self, root="./NLGraph", task="connectivity", split="train", string_only=False):
         with open(root + "/" + task + "/" + split + ".json") as f:
             self.data = json.load(f)
+
+        self.labels = []
+        self.string_data = [] # full string (original data from NLGraph)
+        self.graph_data = [] # parsed set of edges
+        self.context_data = [] # context string - i.e., description of question
+
+        keys = list(self.data.keys()) # maintaining same ordering
+        for key in keys:
+            data_string = self.data[key]["question"]
+            self.string_data.append(data_string)
+            self.labels.append(self.data[key]["answer"])
+
+        # only returning string info
+        if string_only:
+            self.graph_data = self.string_data
+            self.context_data = self.string_data
+        else:
+            for key in keys:
+                data_string = self.data[key]["question"]
+                graph, context = self.convert_string(data_string)
+                self.graph_data.append(graph)
+                self.context_data.append(context)
+
+    def convert_string(self, string, ):
+        '''
+        Convert string into graph data structure representation
+        '''
+
+        # get index of Graph:
+        # graph_index = string.find("Graph: ")
+        graph_index = string.find(": ")
+        graph_end_index = string.find("\nQ:")
+        graph_string = string[graph_index + 3:graph_end_index]
+        context_string = (string[:graph_index], string[graph_end_index:])
+
+        graph = set()
+        edges_string = graph_string.split(" ") 
+        for edge in edges_string:
+            # remove parens
+            edge = edge.replace("(", "").replace(")", "")
+            edge = edge.split(",")
+            graph.add((int(edge[0]), int(edge[1])))
+        return graph, context_string
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, index):
-        index_string = str(index)
-        return self.data[index_string]["question"], self.data[index_string]["answer"]
+        return self.graph_data[index], self.context_data[index], self.labels[index]
+
 
 if __name__ == '__main__':
-    # dataset = NLGraph(root="./NLGraph", task="connectivity", split="train")
+    dataset = NLGraph(root="./NLGraph", task="connectivity", split="train")
     # dataset = NLGraph(root="./NLGraph", task="cycle", split="train")
-    # dataset = NLGraph(root="./NLGraph", task="topology", split="train")
-    dataset = ArxivDataset(root="./ogbn-arxiv")
+    
+    # dataset = ArxivDataset(root="./ogbn-arxiv")
     print(dataset[0])
